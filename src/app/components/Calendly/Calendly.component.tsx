c3937086d6e354c707fb09327d410c5c8a8ca6e8
Michael Kanahele <michael@fitscale.net>
2023-10-14 15:33:43 -0700

    Test

--- src/app/components/Calendly/Calendly.component.tsx
+++ src/app/components/Calendly/Calendly.component.tsx
@@ -11,7 +11,7 @@ const Calendly = () => {
         height: "800px",
         transform: desktop ? "translateY(-21px)" : "translateY(5px)",
       }}
-      url="https://calendly.com/fitscale/30min"
+      url="https://calendly.com/fitscale/15-min"
     />
   );
 };
