31ed8a02137f5c789216670584de3015d2723ce9
Michael Kanahele <michael@fitscale.net>
2023-10-20 20:12:48 -0700

    Test

--- src/app/components/MuxVideo/MuxVideo.component.tsx
+++ src/app/components/MuxVideo/MuxVideo.component.tsx
@@ -71,7 +71,7 @@ const MuxVideo = ({ config }: { config: MuxVideoProps }) => {
           </div>
           <div className={style.videoContainer}>
             <MuxPlayer
-              poster="https://image.mux.com/hxn02MLOAICEZZAiJNOw5MXGFUao9Dm3B4GcqfntA01fY/thumbnail.png?time=0.075"
+              poster="https://image.mux.com/hxn02MLOAICEZZAiJNOw5MXGFUao9Dm3B4GcqfntA01fY/animated.gif?width=630&start=0&end=10&fps=30"
               streamType="on-demand"
               playbackId={config.videoId}
               metadata={{
