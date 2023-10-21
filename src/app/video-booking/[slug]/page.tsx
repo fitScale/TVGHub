0556a84893465f7e310d9ff9ef4fdf3cfae52083
Michael Kanahele <michael@fitscale.net>
2023-10-20 17:50:26 -0700

    Test

--- src/app/video-booking/[slug]/page.tsx
+++ src/app/video-booking/[slug]/page.tsx
@@ -7,8 +7,8 @@ import ImageContainer, {
 import { PortableText } from "@portabletext/react";
 import Calendly from "@/app/components/Calendly/Calendly.component";
 import Svg from "../../../../public/svgs/svgComponent/svg.component";
-// export const dynamic = "force-dynamic";
-// export const revalidate = 1;
+export const dynamic = "force-dynamic";
+export const revalidate = 1;
 
 const client = createClient({
   projectId: "xo86se8r",
