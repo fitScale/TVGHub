c0001088a86c0f30ea6b733896a09b9b606c426f
Michael Kanahele <michael@fitscale.net>
2023-11-08 16:32:22 -0800

    Test

--- src/app/components/Calendly/Calendly.component.tsx
+++ src/app/components/Calendly/Calendly.component.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import { InlineWidget } from "react-calendly";
 
-const Calendly = () => {
-  const desktop = window.innerWidth > 943;
+const Calendly: React.FC = () => {
+  const [isDesktop, setIsDesktop] = useState<boolean>(false);
+
+  useEffect(() => {
+    // This code runs when the component mounts, and window is available
+    const handleResize = () => setIsDesktop(window.innerWidth > 943);
+    // Set the initial value
+    handleResize();
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <InlineWidget
       styles={{
         height: "800px",
-        transform: desktop ? "translateY(-21px)" : "translateY(5px)",
+        transform: isDesktop ? "translateY(-21px)" : "translateY(5px)",
       }}
       url="https://calendly.com/fitscale/15-min"
     />
